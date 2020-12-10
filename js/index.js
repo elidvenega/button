8de4ac003535f3bfb0006e66e4e8d31b8bdfb115
elid venega <venegaelid@gmail.com>
2020-12-09 21:51:03 -0800

    button

--- js/index.js
+++ js/index.js
@@ -3,4 +3,18 @@ const btn = document.querySelector("#btn");
 function msg(message) {
     btn.innerHTML = message;
 }
+
 btn.addEventListener("click", () => msg("hello"));
+
+// Second Way of doing things
+// btn.addEventListener("click", function () {
+//    alert("hello");
+// });
+
+
+
+// btn.addEventListener("click", greet);
+
+// function greet() {
+//     alert("hello world");
+// }
